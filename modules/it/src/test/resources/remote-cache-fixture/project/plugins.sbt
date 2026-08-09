653796058088f27f46e9518c8608634e18d8f6a9
Russ White <356303+russwyte@users.noreply.github.com>
2026-08-09 07:01:32 -0500

    Bump sbt to 2.0.6 (GHSA-m2pw-22cj-jq4v) and combine dependency updates (#63)
    * Update early-effect-docs-theme to 0.12.0
    
    (cherry picked from commit 58bcbb184eb3c5754f7a5977070d8702a803cbbd)
    
    * Update sbt-specular to 0.12.0
    
    (cherry picked from commit 370dfcb50c30e0592534029021095bebf926cbd3)
    
    * Update sbt to 2.0.6
    
    (cherry picked from commit 8476d6fa0e0ed13b7f693cfb1ebf91825457e4b5)
    
    * Update sbt-remote-cache to 2.0.6
    
    (cherry picked from commit ce1139221c3622c9dcc998fc99caa0d81c0b2aba)
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- modules/it/src/test/resources/remote-cache-fixture/project/plugins.sbt
+++ modules/it/src/test/resources/remote-cache-fixture/project/plugins.sbt
@@ -1,1 +1,1 @@
-addSbtPlugin("org.scala-sbt" % "sbt-remote-cache" % "2.0.5")
+addSbtPlugin("org.scala-sbt" % "sbt-remote-cache" % "2.0.6")
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies:
 
   val scala3Version      = "3.8.4"
   val zioVersion         = "2.1.26"
-  val specularVersion    = "0.11.0"
+  val specularVersion    = "0.12.0"
   val zioBlocksVersion   = "0.0.51"
   val remoteCacheVersion = "2.0.5"
   val neotypeVersion     = "0.7.0"
