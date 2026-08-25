dd818d74f7f1cdec7590c1404eb5e69d275d15b8
Russ White <356303+russwyte@users.noreply.github.com>
2026-08-25 18:34:53 -0500

    Bump sbt to 2.0.7 and regenerate workflows (#137)

--- project/ZipxVersions.scala
+++ project/ZipxVersions.scala
@@ -8,7 +8,7 @@ import zipx.*
   */
 object ZipxVersions extends zipx.ZipxVersions:
 
-  val sbt: SbtVersion     = SbtVersion("2.0.6")
+  val sbt: SbtVersion     = SbtVersion("2.0.7")
   val scala: ScalaVersion = ScalaVersion("3.8.4")
 
   val zio: Lib            = Lib("dev.zio", "zio", "2.1.26")
